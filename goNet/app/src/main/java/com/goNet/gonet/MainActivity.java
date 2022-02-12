cf245874f1f8678184227148ef3132df18aefecf
DIGIMAT <36770624+DIGIMATWEB@users.noreply.github.com>
2022-02-12 11:43:13 -0600

    setup splash
    -setup splash like netflix

--- goNet/app/src/main/java/com/goNet/gonet/MainActivity.java
+++ goNet/app/src/main/java/com/goNet/gonet/MainActivity.java
@@ -2,13 +2,59 @@ package com.goNet.gonet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.ValueAnimator;
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
+
+import com.airbnb.lottie.LottieAnimationView;
+import com.goNet.gonet.secondactivity.view.secondActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private  MediaPlayer song1 = new MediaPlayer();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LottieAnimationView animationView= findViewById(R.id.splash);
+
+
+
+        song1 = MediaPlayer.create(getApplicationContext(),  R.raw.netflixsound);
+        song1.setLooping(false);
+        animationView.addAnimatorUpdateListener(
+                        (animation) -> {
+                            // Do something.
+                            song1.start();
+
+
+                        });
+        animationView.playAnimation();
+
+        if (animationView.isAnimating()) {
+
+        }else
+        {
+
+            new Handler().postDelayed(new Runnable() {
+
+                @Override
+                public void run() {
+                    song1.pause();
+                    goToLoginContainer();
+
+                }
+            },4000);
+        }
+    }
+    private void goToLoginContainer() {
+        Intent intent = new Intent(MainActivity.this, secondActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+        finish();
+
     }
 }
---
+++ goNet/app/src/main/java/com/goNet/gonet/secondactivity/view/secondActivity.java
@@ -0,0 +1,15 @@
+package com.goNet.gonet.secondactivity.view;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.goNet.gonet.R;
+
+public class secondActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_activity);
+    }
+}
